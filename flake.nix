dbc433001f8cce4a01e6effbddddfe03ddaaa25c
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2024-05-08 15:40:12 -0400

    Updated flake

--- flake.nix
+++ flake.nix
@@ -6,12 +6,28 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/default/configuration.nix
-      ];
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
+    
+    # Systems 
+    nixosConfigurations = {
+      
+      default = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/default/configuration.nix
+        ];
+      };
+
+      msi = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          nixos-hardware.nixosModules.ms-gs60
+          # nixos-hardware.nixosModules.common-gpu-nvidia
+          ./hosts/default/configuration.nix
+        ];
+      };
+
     };
+
   };
 }
