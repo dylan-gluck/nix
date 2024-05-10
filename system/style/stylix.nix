1d51405cdea9e37aeedce90398b4275949923c45
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2024-05-10 01:02:59 -0400

    Updates stylix implementation

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -1,24 +1,18 @@
 { config, lib, pkgs, inputs, ... }:
 
-let
-  themePath = "../../../themes/nord/nord.yaml";
-  themePolarity = "dark";
-  myLightDMTheme = if themePolarity == "light" then "Adwaita" else "Adwaita-dark";
-  backgroundUrl = builtins.readFile (./. + "../../../themes/nord/backgroundurl.txt");
-  backgroundSha256 = builtins.readFile (./. + "../../../themes/nord/backgroundsha256.txt");
-in
 {
   imports = [ inputs.stylix.nixosModules.stylix ];
 
+  imports = [ inputs.stylix.nixosModules.stylix ];
+
   stylix.autoEnable = false;
-  stylix.polarity = themePolarity;
-  stylix.image = pkgs.fetchurl {
-   url = backgroundUrl;
-   sha256 = backgroundSha256;
-  };
   
-  stylix.base16Scheme = ./. + themePath;
-  
+  stylix.polarity = "dark";
+
+  stylix.image = ../../wallpaper/bg.jpg;
+
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
+
   stylix.fonts = {
     monospace = {
       name = "Intel One Mono";
@@ -36,14 +30,34 @@ in
       name = "Noto Color Emoji";
       package = pkgs.noto-fonts-emoji-blob-bin;
     };
+    sizes = {
+      terminal = 12;
+      applications = 12;
+      popups = 12;
+      desktop = 12;
+    };
   };
 
+  stylix.targets.console.enable = true;
   stylix.targets.lightdm.enable = true;
+
+  stylix.targets.kitty.enable = true;
+  stylix.targets.alacritty.enable = true;
+  
+  stylix.targets.i3.enable = true;
+  stylix.targets.kde.enable = true;
+  stylix.targets.gnome.enable = true;
+  stylix.targets.gtk.enable = true;
+  
+  stylix.targets.rofi.enable = true;
+  stylix.targets.feh.enable = true;
+
+  programs.feh.enable = true;
+
   services.xserver.displayManager.lightdm = {
       greeters.slick.enable = true;
       greeters.slick.theme.name = myLightDMTheme;
   };
-  stylix.targets.console.enable = true;
 
   environment.sessionVariables = {
     QT_QPA_PLATFORMTHEME = "qt5ct";
--- user/style/stylix.nix
+++ user/style/stylix.nix
@@ -1,73 +1,21 @@
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, ... }:
 
-let
-  themePath = "../../../themes/nord/nord.yaml";
-  themePolarity = "dark";
-  backgroundUrl = builtins.readFile (./. + "../../../themes/nord/backgroundurl.txt");
-  backgroundSha256 = builtins.readFile (./. + "../../../themes/nord/backgroundsha256.txt");
-in
 {
 
-  imports = [ inputs.stylix.nixosModules.stylix ];
-
-  home.file.".currenttheme".text = "nord";
-
-  stylix.autoEnable = false;
-  stylix.polarity = themePolarity;
-
-  stylix.image = pkgs.fetchurl {
-    url = backgroundUrl;
-    sha256 = backgroundSha256;
-  };
-
-  stylix.base16Scheme = ./. + themePath;
-
-  stylix.fonts = {
-    monospace = {
-      name = "Intel One Mono";
-      package = pkgs.intel-one-mono;
-    };
-    serif = {
-      name = "Intel One Mono";
-      package = pkgs.intel-one-mono;
-    };
-    sansSerif = {
-      name = "Intel One Mono";
-      package = pkgs.intel-one-mono;
-    };
-    emoji = {
-      name = "Noto Color Emoji";
-      package = pkgs.noto-fonts-emoji-blob-bin;
-    };
-    sizes = {
-      terminal = 12;
-      applications = 12;
-      popups = 12;
-      desktop = 12;
-    };
-  };
-
-  stylix.targets.i3.enable = true;
-  stylix.targets.kitty.enable = true;
-  stylix.targets.alacritty.enable = true;
-  stylix.targets.kde.enable = true;
-  stylix.targets.gnome.enable = true;
-  stylix.targets.gtk.enable = true;
-  stylix.targets.rofi.enable = true;
-  stylix.targets.feh.enable = true;
-
-  programs.feh.enable = true;
+  home.file.".currenttheme".text = "tokyo-night-storm";
 
   home.file.".fehbg-stylix".text = ''
     #!/bin/sh
     feh --no-fehbg --bg-fill ''+config.stylix.image+'';
   '';
   home.file.".fehbg-stylix".executable = true;
+  
   home.file.".swaybg-stylix".text = ''
     #!/bin/sh
     swaybg -m fill -i ''+config.stylix.image+'';
   '';
   home.file.".swaybg-stylix".executable = true;
+  
   home.file.".swayidle-stylix".text = ''
     #!/bin/sh
     swaylock_cmd='swaylock --indicator-radius 200 --screenshots --effect-blur 10x10'
@@ -77,6 +25,7 @@ in
                 before-sleep "$swaylock_cmd"
     '';
   home.file.".swayidle-stylix".executable = true;
+  
   home.file = {
     ".config/qt5ct/colors/oomox-current.conf".source = config.lib.stylix.colors {
       template = builtins.readFile ./oomox-current.conf.mustache;
@@ -92,9 +41,11 @@ in
     };
     ".config/qt5ct/qt5ct.conf".text = pkgs.lib.mkBefore (builtins.readFile ./qt5ct.conf);
   };
+  
   home.packages = with pkgs; [
      libsForQt5.qt5ct pkgs.libsForQt5.breeze-qt5 libsForQt5.breeze-icons
   ];
+  
   qt = {
     enable = true;
     style.package = pkgs.libsForQt5.breeze-qt5;
