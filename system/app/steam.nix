8760040df81e7d36c7ff885f1c59489964c3bbfa
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2024-05-10 13:16:17 -0400

    Adds proton support for steam

--- system/app/steam.nix
+++ system/app/steam.nix
@@ -1,7 +1,18 @@
 { pkgs, ... }:
 
 {
-  hardware.opengl.driSupport32Bit = true;
-  programs.steam.enable = true;
-  environment.systemPackages = [ pkgs.steam ];
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    steam
+    mangohud
+    protonup
+  ];
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATH = "/home/d/.steam/root/compatabilitytools.d";
+  };
 }
