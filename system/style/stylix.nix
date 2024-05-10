24b0840ac34a33ff53fb145d564acfc5e3c04888
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2024-05-10 01:11:55 -0400

    Trying to get stylix working

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -3,7 +3,7 @@
 {
   imports = [ inputs.stylix.nixosModules.stylix ];
 
-  stylix.autoEnable = false;
+  stylix.autoEnable = true;
   
   stylix.polarity = "dark";
 
@@ -35,20 +35,6 @@
       desktop = 12;
     };
   };
-
-  stylix.targets.console.enable = true;
-  stylix.targets.lightdm.enable = true;
-
-  stylix.targets.kitty.enable = true;
-  stylix.targets.alacritty.enable = true;
-  
-  stylix.targets.i3.enable = true;
-  stylix.targets.kde.enable = true;
-  stylix.targets.gnome.enable = true;
-  stylix.targets.gtk.enable = true;
-  
-  stylix.targets.rofi.enable = true;
-  stylix.targets.feh.enable = true;
 
   programs.feh.enable = true;
 
--- user/style/stylix.nix
+++ user/style/stylix.nix
@@ -10,37 +10,37 @@
   '';
   home.file.".fehbg-stylix".executable = true;
   
-  # home.file.".swaybg-stylix".text = ''
-  #   #!/bin/sh
-  #   swaybg -m fill -i ''+config.stylix.image+'';
-  # '';
-  # home.file.".swaybg-stylix".executable = true;
+  home.file.".swaybg-stylix".text = ''
+    #!/bin/sh
+    swaybg -m fill -i ''+config.stylix.image+'';
+  '';
+  home.file.".swaybg-stylix".executable = true;
   
-  # home.file.".swayidle-stylix".text = ''
-  #   #!/bin/sh
-  #   swaylock_cmd='swaylock --indicator-radius 200 --screenshots --effect-blur 10x10'
-  #   swayidle -w timeout 300 "$swaylock_cmd --fade-in 0.5 --grace 5" \
-  #               timeout 600 'hyprctl dispatch dpms off' \
-  #               resume 'hyprctl dispatch dpms on' \
-  #               before-sleep "$swaylock_cmd"
-  #   '';
-  # home.file.".swayidle-stylix".executable = true;
+  home.file.".swayidle-stylix".text = ''
+    #!/bin/sh
+    swaylock_cmd='swaylock --indicator-radius 200 --screenshots --effect-blur 10x10'
+    swayidle -w timeout 300 "$swaylock_cmd --fade-in 0.5 --grace 5" \
+                timeout 600 'hyprctl dispatch dpms off' \
+                resume 'hyprctl dispatch dpms on' \
+                before-sleep "$swaylock_cmd"
+    '';
+  home.file.".swayidle-stylix".executable = true;
   
-  # home.file = {
-  #   ".config/qt5ct/colors/oomox-current.conf".source = config.lib.stylix.colors {
-  #     template = builtins.readFile ./oomox-current.conf.mustache;
-  #     extension = ".conf";
-  #   };
-  #   ".config/Trolltech.conf".source = config.lib.stylix.colors {
-  #     template = builtins.readFile ./Trolltech.conf.mustache;
-  #     extension = ".conf";
-  #   };
-  #   ".config/kdeglobals".source = config.lib.stylix.colors {
-  #     template = builtins.readFile ./Trolltech.conf.mustache;
-  #     extension = "";
-  #   };
-  #   ".config/qt5ct/qt5ct.conf".text = pkgs.lib.mkBefore (builtins.readFile ./qt5ct.conf);
-  # };
+  home.file = {
+    ".config/qt5ct/colors/oomox-current.conf".source = config.lib.stylix.colors {
+      template = builtins.readFile ./oomox-current.conf.mustache;
+      extension = ".conf";
+    };
+    ".config/Trolltech.conf".source = config.lib.stylix.colors {
+      template = builtins.readFile ./Trolltech.conf.mustache;
+      extension = ".conf";
+    };
+    ".config/kdeglobals".source = config.lib.stylix.colors {
+      template = builtins.readFile ./Trolltech.conf.mustache;
+      extension = "";
+    };
+    ".config/qt5ct/qt5ct.conf".text = pkgs.lib.mkBefore (builtins.readFile ./qt5ct.conf);
+  };
   
   home.packages = with pkgs; [
      libsForQt5.qt5ct pkgs.libsForQt5.breeze-qt5 libsForQt5.breeze-icons
