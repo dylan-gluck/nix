05ff42504db28e56c5802d248576a5532f8916de
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2024-05-10 01:23:57 -0400

    Trying to fix config

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -1,38 +1,34 @@
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  imports = [ inputs.stylix.nixosModules.stylix ];
-
-  stylix.autoEnable = true;
-  
-  stylix.polarity = "dark";
-
-  stylix.image = ../../wallpaper/bg.jpg;
-
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
-
-  stylix.fonts = {
-    monospace = {
-      name = "Intel One Mono";
-      package = pkgs.intel-one-mono;
-    };
-    serif = {
-      name = "Intel One Mono";
-      package = pkgs.intel-one-mono;
-    };
-    sansSerif = {
-      name = "Intel One Mono";
-      package = pkgs.intel-one-mono;
-    };
-    emoji = {
-      name = "Noto Color Emoji";
-      package = pkgs.noto-fonts-emoji-blob-bin;
-    };
-    sizes = {
-      terminal = 12;
-      applications = 12;
-      popups = 12;
-      desktop = 12;
+  stylix = {
+    autoEnable = true;
+    polarity = "dark";
+    image = ../../wallpaper/bg.jpg;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-storm.yaml";
+    fonts = {
+      monospace = {
+        name = "Intel One Mono";
+        package = pkgs.intel-one-mono;
+      };
+      serif = {
+        name = "Intel One Mono";
+        package = pkgs.intel-one-mono;
+      };
+      sansSerif = {
+        name = "Intel One Mono";
+        package = pkgs.intel-one-mono;
+      };
+      emoji = {
+        name = "Noto Color Emoji";
+        package = pkgs.noto-fonts-emoji-blob-bin;
+      };
+      sizes = {
+        terminal = 12;
+        applications = 12;
+        popups = 12;
+        desktop = 12;
+      };
     };
   };
 
