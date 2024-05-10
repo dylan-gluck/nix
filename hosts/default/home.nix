41046bfd522312e7fb0dc8587baae85807763e31
Dylan Navajas Gluck <dylan.n.gluck@gmail.com>
2024-05-10 02:15:36 -0400

    Trying to fix config

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -4,7 +4,7 @@
   # Home Manager setup
   home.username = "d";
   home.homeDirectory = "/home/d";
-  home.stateVersion = "23.11";
+  home.stateVersion = "22.11";
 
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
